b5c66e693dad9cd9820d4db3abe609510445c8d0
xavierdeoliveiramauricio@gmail.com <xavierdeoliveiramauricio@gmail.com>
2025-07-13 19:22:25 -0300

    update database connection

--- models/database.js
+++ models/database.js
@@ -7,6 +7,7 @@ async function getNewClient() {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
+    ssl: process.env.NODE_ENV === "production",
   });
 
   await client.connect();
